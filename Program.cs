a94d262c6b28f61992b6b1dced0de8fa82de452d
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-27 12:44:37 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackAndQueueProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine ("Welcome to Stack and queue prblems");
+        }
+    }
+}
